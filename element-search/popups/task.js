1fa6b0bc56ed13bb606d4e1eb09ef3eae433f8ea
LeoSibiryakov <54851690+LeoSibiryakov@users.noreply.github.com>
2020-09-26 12:35:56 +0500

    Add files via upload

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,23 +1,17 @@
 const modalMain = document.getElementById('modal_main'); // находим первый выпадающий попап с красной кнопкой
-const modalClose = document.getElementsByClassName('modal__close_times'); // коллекция элементов-крестиков
-const showSuccess = document.getElementsByClassName('show-success'); //  коллекция красной кнопки
-const success = document.getElementById('modal_success'); // второй попап с зелёной кнопкой 
+const success = document.getElementById('modal_success'); // второй попап с зелёной кнопкой
+const modalClose = document.querySelectorAll('.modal__close'); // элементы закрытия
+const showSuccess = document.querySelector('.show-success'); //  красная кнопка
 
-const firstClose = modalClose.item(0); // крестик закрытия окна 1 попапа
-const secondClose = modalClose.item(1); // крестик закрытия окна 2 попапа
-const redPopup = showSuccess.item(0);  // кнопка красная сделать хорошо
+modalMain.classList.add('modal_active'); 
 
-modalMain.classList.add('modal_active'); // добавляем класс с первым выпадающий попапом.
+for (let i = 0; i < modalClose.length;i++) {
+    modalClose[i].addEventListener('click', function() {
+        modalMain.classList.remove('modal_active');
+        success.classList.remove('modal_active');
+    })
+}
 
-firstClose.onclick = function() {
-    modalMain.classList.remove('modal_active'); // при нажатии на первый крестик скрываем первый попап
-};
-
-redPopup.onclick = function () { // при нажатии на красную кнопку
-    modalMain.classList.remove('modal_active');  // удалаем первый попап
-    success.classList.add('modal_active'); // и добавляем второй
-};
-
-secondClose.onclick = function() { // при нажатии на второй крестик 
-    success.classList.remove('modal_active'); // удаляем второй попап
-};
+showSuccess.onclick = function() {
+    success.classList.add('modal_active');
+}
